b1601e40c524c19cbb65ac9ddd6041b375190039
Ruan246Etec <ruan.projeto3@gmail.com>
2025-11-07 03:19:20 +0000

    terminando, só falta a imagem do inicio que da preta ainda

--- screens/TelaCatalogo.jsx
+++ screens/TelaCatalogo.jsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 import vinhoBranco from '../assets/vinho-branco.jpg';
 import vinhoRose from '../assets/vinho-rose.jpg';
@@ -7,52 +7,118 @@ import vinhoEspecial from '../assets/vinho-especial.jpg';
 
 export default function TelaCatalogo() {
   return (
-    <View style={styles.container}>
-        <Text>Nossos vinhos</Text>
-        <Text>Trabalhamos com o melhor vinho dos seguintes tipos: Vinho branco, vinho rosé, vinho tinto e vinho seco.</Text>
-        
-        <View>
-            <Image 
+    <ScrollView contentContainerStyle={styles.scrollContent}>
+
+      <View style={styles.container}>
+        <Text style={styles.titulo}>Nossos vinhos</Text>
+        <Text style={styles.subtitulo}>Trabalhamos com o melhor vinho dos seguintes tipos: Vinho branco, vinho rosé, vinho tinto e vinho seco.</Text>
+
+        <View style={styles.vinhoContainer}>
+          <Image 
             source={vinhoBranco}
-            />
-            <Text>Chatigny Chardonnay</Text>
-            <Text>Vinho leve, refrescante e levemente cítrico da cor amarelo palha. Perfeito com carnes brancas e massa ao pesto.</Text>
+            style={styles.vinhoImage}
+          />
+          <View style={styles.vinhoText}>
+            <Text style={styles.vinho}>Chatigny Chardonnay</Text>
+            <Text style={styles.vinhoDesc}>Vinho leve, refrescante e levemente cítrico da cor amarelo palha. Perfeito com carnes brancas e massa ao pesto.</Text>
+          </View>
         </View>
-        
-        <View>
-            <Image 
+
+        <View style={styles.vinhoContainer}>
+          <Image 
             source={vinhoRose}
-            />
-            <Text>Concha y Toro Exportacion</Text>
-            <Text>Vinho rosé fresco, intenso e macio da cor rosa pálido. Perfeito com saladas e aperitivos.</Text>
+            style={styles.vinhoImage}
+          />
+          <View style={styles.vinhoText}>
+            <Text style={styles.vinho}>Concha y Toro Exportacion</Text>
+            <Text style={styles.vinhoDesc}>Vinho rosé fresco, intenso e macio da cor rosa pálido. Perfeito com saladas e aperitivos.</Text>
+          </View>
         </View>
-        
-        <View>
-            <Image 
+
+        <View style={styles.vinhoContainer}> 
+          <Image 
             source={vinhoSeco}
-            />
-            <Text>Portada Winemaker's</Text>
-            <Text>Vinho encorpado, saboroso e frutado, com final levemente adocicado. Sua cor é vermelho-rubi.Perfeito com queijo parmesão e carnes assadas ou grelhadas.</Text>
+            style={styles.vinhoImage}
+          />
+          <View style={styles.vinhoText}>
+            <Text style={styles.vinho}>Portada Winemaker's</Text>
+            <Text style={styles.vinhoDesc}>Vinho encorpado, saboroso e frutado, com final levemente adocicado. Sua cor é vermelho-rubi. Perfeito com queijo parmesão e carnes assadas ou grelhadas.</Text>
+          </View>
         </View>
-        
-        <View>
-            <Image 
+
+        <View style={styles.vinhoContainer}>
+          <Image 
             source={vinhoEspecial}
-            />
-            <Text>Elvio Cogno Ravera Barolo</Text>
-            <Text>Vinho estruturado, com sabor de cereja vermelha madura, framboesa, notas de tabaco e taninos aveludados. Sua cor é vermelho granada e é perfeito com carnes vermelhas e molhos encorpados.</Text>
+            style={styles.vinhoImage}
+          />
+          <View style={styles.vinhoText}>
+            <Text style={styles.vinho}>Elvio Cogno Ravera Barolo</Text>
+            <Text style={styles.vinhoDesc}>Vinho estruturado, com sabor de cereja vermelha madura, framboesa, notas de tabaco e taninos aveludados. Sua cor é vermelho granada e é perfeito com carnes vermelhas e molhos encorpados.</Text>
+          </View>
         </View>
 
-
-    </View>
+      </View>
+    </ScrollView>
   );
 }
-
+ 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  titulo: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginBottom: 6,
+    alignSelf: 'flex-start',
+    marginLeft: 20,
+  },
+  subtitulo: {
+    fontSize: 16,
+    textAlign: 'left',
+    color: '#5b5b5b',
+    marginBottom: 20,
+    paddingHorizontal: 20,
+    alignSelf: 'flex-start',
+    marginLeft: 20,
+  },
+  vinho: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginTop: 0,
+    color: '#fff',
+  },
+  vinhoDesc: {
+    fontSize: 15,
+    textAlign: 'left',
+    marginBottom: 0,
+    paddingRight: 6,
+    color: '#f5f1ee',
+    lineHeight: 18,
+  },
+  vinhoContainer: {
+    marginBottom: 20,
+    backgroundColor: '#b88f86',
+    width: '96%',
+    height: 160,
+    borderRadius: 14,
+    padding: 16,
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  vinhoImage: {
+    width: 100,
+    height: 150,
+    resizeMode: 'contain',
+    marginRight: 12,
+    borderRadius: 4,
+    backgroundColor: 'transparent'
+  },
+  vinhoText: {
+    flex: 1,
+  },
+  scrollContent: {
+    paddingBottom: 40,
+    paddingLeft: 20,
+    backgroundColor: '#fff',
+    alignItems: 'center'
   },
 });
--- screens/TelaContato.jsx
+++ screens/TelaContato.jsx
@@ -1,37 +1,39 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { FontAwesome, Entypo, MaterialIcons } from '@expo/vector-icons';
 
-export default function TelaContato(){
-    return(
-        <View>
-            <Text>Entre em contato conosco para comprar nossos produtos</Text>
+export default function TelaContato() {
+    return (
+        <ScrollView contentContainerStyle={styles.scrollContent}>
+            <View>
+                <Text style={styles.titulo}>Entre em contato conosco para comprar nossos produtos</Text>
 
-            <View style={styles.contatoLista}>
-                <View style={styles.contatoBox}>
-                    <FontAwesome name="phone" size={50} color="#400303" />
-                    <Text style={styles.contatoTitulo}>Telefone:</Text>
-                    <Text style={styles.contatoParagrafo}>+55 21 00000-0000</Text>
-                </View>
-                
-                <View style={styles.contatoBox}>
-                    <Entypo name="location-pin" size={50} color="#400303" />
-                    <Text style={styles.contatoTitulo}>Endereço:</Text>
-                    <Text style={styles.contatoParagrafo}>Av. 123, 222 - Rio de Janeiro RJ </Text>
-                </View>
-                
-                <View style={styles.contatoBox}>
-                    <MaterialIcons name="email" size={50} color="#400303" />
-                    <Text style={styles.contatoTitulo}>Email:</Text>
-                    <Text style={styles.contatoParagrafo}>preferida@adega.com.br</Text>
-                </View>
-                
-                <View style={styles.contatoBox}>
-                    <FontAwesome name="instagram" size={50} color="#400303" />
-                    <Text style={styles.contatoTitulo}>Instagram:</Text>
-                    <Text style={styles.contatoParagrafo}>@adegapreferida</Text>
+                <View style={styles.contatoLista}>
+                    <View style={styles.contatoBox}>
+                        <FontAwesome name="phone" size={50} color="#4b0d0d" />
+                        <Text style={styles.contatoTitulo}>Telefone:</Text>
+                        <Text style={styles.contatoParagrafo}>+55 21 00000-0000</Text>
+                    </View>
+
+                    <View style={styles.contatoBox}>
+                        <Entypo name="location-pin" size={50} color="#4b0d0d" />
+                        <Text style={styles.contatoTitulo}>Endereço:</Text>
+                        <Text style={styles.contatoParagrafo}>Av. 123, 222 - Rio de Janeiro RJ</Text>
+                    </View>
+
+                    <View style={styles.contatoBox}>
+                        <MaterialIcons name="email" size={50} color="#4b0d0d" />
+                        <Text style={styles.contatoTitulo}>Email:</Text>
+                        <Text style={styles.contatoParagrafo}>preferida@adega.com.br</Text>
+                    </View>
+
+                    <View style={styles.contatoBox}>
+                        <FontAwesome name="instagram" size={50} color="#4b0d0d" />
+                        <Text style={styles.contatoTitulo}>Instagram:</Text>
+                        <Text style={styles.contatoParagrafo}>@adegapreferida</Text>
+                    </View>
                 </View>
             </View>
-        </View>
+        </ScrollView>
     )
 }
 
@@ -40,26 +42,47 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        padding: 20
     },
-
+    titulo: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        marginBottom: 18,
+        textAlign: 'left',
+        alignSelf: 'flex-start',
+        marginLeft: 20,
+    },
     contatoLista: {
-        padding: 16,
+        paddingVertical: 8,
+        width: '100%',
+        alignItems: 'center',
     },
-
     contatoBox: {
         alignItems: 'center',
         marginBottom: 16,
+        borderWidth: 1,
+        borderColor: '#7a7a7aff',
+        backgroundColor: '#FFFFFF',
+        paddingVertical: 18,
+        paddingHorizontal: 14,
+        borderRadius: 10,
+        width: '80%',
     },
-
     contatoTitulo: {
         fontWeight: 'bold',
         fontSize: 16,
         marginTop: 8,
+        color: '#4b0d0d',
     },
-
     contatoParagrafo: {
         fontSize: 14,
-        color: '#333',
+        color: '#4b0d0d',
+        marginTop: 6,
+        textAlign: 'center',
     },
+    scrollContent: {
+        paddingBottom: 40,
+        paddingLeft: 20,
+        backgroundColor: '#fff',
+        alignItems: 'center'
+    }
 });
